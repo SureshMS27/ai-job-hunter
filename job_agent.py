a7d05e7160691810110dbd9b722ea7b5e5f8de61
Devine Aura Events <153260154+SureshMS27@users.noreply.github.com>
2026-03-16 08:39:27 +0530

    Create job_agent.py

---
+++ job_agent.py
@@ -0,0 +1,12 @@
+import requests
+
+print("Scanning Digital Marketing Jobs...")
+
+jobs = [
+    "SEO Executive - Remote",
+    "Digital Marketing Intern - India",
+    "Social Media Manager - Hyderabad"
+]
+
+for job in jobs:
+    print("Found Job:", job)
